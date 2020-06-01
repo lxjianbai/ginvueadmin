fede35c6efea58c4a8db8d442907d86ca7c3fa39
lxjianbai <583502241@qq.com>
2020-06-01 23:39:12 +0800

    init

---
+++ server/model/sys_user.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/satori/go.uuid"
+)
+
+type SysUser struct {
+	gorm.Model
+	UUID        uuid.UUID    `json:"uuid"`
+	Username    string       `json:"userName"`
+	Password    string       `json:"-"`
+	NickName    string       `json:"nickName" gorm:"default:'系统用户'"`
+	HeaderImg   string       `json:"headerImg" gorm:"default:'http://qmplusimg.henrongyi.top/head.png'"`
+	Authority   SysAuthority `json:"authority" gorm:"ForeignKey:AuthorityId;AssociationForeignKey:AuthorityId"`
+	AuthorityId string       `json:"authorityId" gorm:"default:888"`
+}
+
